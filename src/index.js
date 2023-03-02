1d64817dbb3f84b87e0bd3e2ad075f64a48dab4b
Norbert Fodor <norbi.boci@gmail.com>
2023-03-02 15:19:16 +0100

    add splash screen and eslint

--- src/index.js
+++ src/index.js
@@ -1,67 +1,78 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, ipcMain } = require('electron');
 const path = require('path');
+const electronSquirrelStartup = require('electron-squirrel-startup');
 
-const config = require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
-if (config.parsed.DB_FILENAME) {
-  process.env.DB_FILENAME = path.resolve(__dirname, '../', config.parsed.DB_FILENAME);
-}
-if (config.parsed.EXTENSIONS_PATH) {
-  process.env.EXTENSIONS_PATH = path.resolve(__dirname, '../', config.parsed.EXTENSIONS_PATH);
-}
-if (config.parsed.STORAGE_LOCAL_ROOT) {
-  process.env.STORAGE_LOCAL_ROOT = path.resolve(__dirname, '../', config.parsed.STORAGE_LOCAL_ROOT);
-}
-if (config.parsed.PACKAGE_FILE_LOCATION) {
-  process.env.PACKAGE_FILE_LOCATION = path.resolve(__dirname, '../', config.parsed.PACKAGE_FILE_LOCATION);
-}
-
-const directus = require("directus/server");
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (require('electron-squirrel-startup')) {
+if (electronSquirrelStartup) {
   app.quit();
 }
 
-const createWindow = () => {
-  // Create the browser window.
+const prepareDirectus = () => {
+  // eslint-disable-next-line global-require
+  const config = require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
+  if (config.parsed.DB_FILENAME) process.env.DB_FILENAME = path.resolve(__dirname, '../', config.parsed.DB_FILENAME);
+  if (config.parsed.EXTENSIONS_PATH) process.env.EXTENSIONS_PATH = path.resolve(__dirname, '../', config.parsed.EXTENSIONS_PATH);
+  if (config.parsed.STORAGE_LOCAL_ROOT) process.env.STORAGE_LOCAL_ROOT = path.resolve(__dirname, '../', config.parsed.STORAGE_LOCAL_ROOT);
+  if (config.parsed.PACKAGE_FILE_LOCATION) process.env.PACKAGE_FILE_LOCATION = path.resolve(__dirname, '../', config.parsed.PACKAGE_FILE_LOCATION);
+  if (app.isPackaged) process.env.LOG_LEVEL = 'silent';
+
+  // eslint-disable-next-line import/no-unresolved, global-require
+  return require('directus/server');
+};
+
+const createWindow = async () => {
+  let isMainWindowVisible = false;
+  const handleHideSplashScreen = (splashScreen, mainWindow) => {
+    if (!isMainWindowVisible) {
+      isMainWindowVisible = true;
+      return;
+    }
+    splashScreen.close();
+    mainWindow.show();
+    mainWindow.focus();
+  };
+
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, "preload.js"),
-      nodeIntegration: true
-    },
+    show: false,
   });
 
-  // and load the index.html of the app.
-  // mainWindow.loadFile(path.join(__dirname, 'index.html'));
-  mainWindow.loadURL("http://localhost:8055");
+  const splashScreen = new BrowserWindow({
+    width: 480,
+    height: 270,
+    transparent: true,
+    frame: false,
+    alwaysOnTop: true,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true,
+    },
+  });
+  ipcMain.handle('onSplashEnded', () => {
+    handleHideSplashScreen(splashScreen, mainWindow);
+  });
+  mainWindow.once('ready-to-show', () => {
+    handleHideSplashScreen(splashScreen, mainWindow);
+  });
 
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  splashScreen.loadFile(path.join(__dirname, 'splash.html'));
+  await prepareDirectus().startServer();
+  mainWindow.loadURL('http://localhost:8055');
+
+  if (!app.isPackaged) mainWindow.webContents.openDevTools();
 };
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.whenReady().then(async () => {
-  await directus.startServer();
   createWindow();
 
-  app.on("activate", () => {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
+  app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) createWindow();
   });
 });
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit();
   }
 });
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.
--- src/preload.js
+++ src/preload.js
@@ -1,2 +1,6 @@
-// See the Electron documentation for details on how to use preload scripts:
-// https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('electronAPI', {
+  send: (channel, data) => ipcRenderer.invoke(channel, data),
+  on: (channel, callable, event, data) => ipcRenderer.on(channel, callable(event, data)),
+});
